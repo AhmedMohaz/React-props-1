da9ab1cb82d5a38791c5daf53887ba7f4d070b88
Ahmed Mohamed <itslokin5@gmail.com>
2025-08-02 11:53:55 +0300

    Create players.js

---
+++ players.js
@@ -0,0 +1,56 @@
+const players = [
+  {
+    name: "Kylian Mbappé",
+    team: "Paris Saint-Germain",
+    nationality: "France",
+    jerseyNumber: 7,
+    age: 25,
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Kylian_Mbappé_2019.jpg/330px-Kylian_Mbappé_2019.jpg",
+    stats: {
+      goals: 180,
+      assists: 80,
+      appearances: 220
+    }
+  },
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami",
+    nationality: "Argentina",
+    jerseyNumber: 10,
+    age: 36,
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Lionel_Messi_20180626.jpg/330px-Lionel_Messi_20180626.jpg",
+    stats: {
+      goals: 804,
+      assists: 350,
+      appearances: 1040
+    }
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Al-Nassr",
+    nationality: "Portugal",
+    jerseyNumber: 7,
+    age: 39,
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Cristiano_Ronaldo_2018.jpg/330px-Cristiano_Ronaldo_2018.jpg",
+    stats: {
+      goals: 865,
+      assists: 270,
+      appearances: 1180
+    }
+  },
+  {
+    name: "Erling Haaland",
+    team: "Manchester City",
+    nationality: "Norway",
+    jerseyNumber: 9,
+    age: 24,
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Erling_Haaland_2021.jpg/330px-Erling_Haaland_2021.jpg",
+    stats: {
+      goals: 130,
+      assists: 40,
+      appearances: 160
+    }
+  }
+];
+
+export default players;
